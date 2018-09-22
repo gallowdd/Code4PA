e3360c9dfcb66f160e2c6bf01baf1990a87d7123
Duncan Campbell <duncan@donut-hole.local>
2018-09-22 10:58:28 -0400

    added source

--- data/download_data.sh
+++ data/download_data.sh
@@ -6,3 +6,5 @@ wget -O EMS_Naloxone_Dose2018.csv https://data.pa.gov/api/views/wst4-3int/rows.c
 # https://dev.socrata.com/foundry/data.pa.gov/p3sw-hiqq
 wget -O Naloxone_Reversals_Law_Enforcement.csv https://data.pa.gov/api/views/e9n3-x87d/rows.csv?accessType=DOWNLOAD&api_foundry=true
 
+# https://catalog.data.gov/dataset/pharmacies-offering-narcan-evzio-and-other-brands-of-naloxone
+wget -O  pharmacies_offering_naloxone.csv https://data.ct.gov/api/views/2vby-9bet/rows.csv?accessType=DOWNLOAD
